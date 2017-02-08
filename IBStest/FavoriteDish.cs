66ac165afb79a0963cbd15c07d7577f3407e4ce3
Igor Gorbunov <give@list.ru>
2017-02-08 18:02:10 +0400

    add datagrid favoriteDishes

---
+++ IBStest/FavoriteDish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBStest
+{
+    class FavoriteDish
+    {
+        [DisplayName("Наименование")]
+        public string Name { get; set; }
+    }
+}
